01f00a477077e3f3c5379d789de3bdda27119658
PRINCE KUMAR DAS <prince30351708@gmail.com>
2025-10-25 10:01:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7026736383-e71c8",
+  "appId": "1:537369066299:web:ab48066718408b5eac151e",
+  "apiKey": "AIzaSyA3jeHaWtHCJgwFHHj3jOjoYqQla3WZdnI",
+  "authDomain": "studio-7026736383-e71c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "537369066299"
+};
